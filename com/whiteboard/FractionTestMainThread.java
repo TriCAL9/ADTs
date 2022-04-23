06ee2ec9289bbc71c3f32f90a49716c13f66455e
Calvin Francis <TriCAL9@users.noreply.github.com>
2022-04-23 12:54:58 -0400

    fraction main
    simple test

---
+++ com/whiteboard/FractionTestMainThread.java
@@ -0,0 +1,14 @@
+package com.whiteboard.number;
+
+import com.whiteboard.number.fraction.Fraction;
+  
+public class FractionTestMainThread {
+  public static void main(String args) {
+    Fraction approximationOfPI= Fraction.newInstance(22, 7);
+    Fraction radius = Fraction.newInstance(3, 1);
+    Fraction areaInsideCircle = radius.times(radius).times(approximationOfPI);
+    System.out.println(areaInsideCircle);
+    System.out.println("Approximated area: " + areaInsideCircle.getNumerator() 
+                       / areaInsideCircle.getDenominator());
+  }
+}
