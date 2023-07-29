452d825c30b8d888fc28a488c19d2dbe52dee7f8
Calvin Francis <calvinfran@gmail.com>
2023-07-29 18:37:35 +0000

    in main method signature a bug was addressed

--- com/whiteboard/number/FractionTestMainThread.java
+++ com/whiteboard/number/FractionTestMainThread.java
@@ -3,7 +3,7 @@ package com.whiteboard.number;
 import com.whiteboard.number.fraction.Fraction;
   
 public class FractionTestMainThread {
-  public static void main(String args) {
+  public static void main(String[] args) {
     Fraction approximationOfPI= Fraction.newInstance(22, 7);
     Fraction radius = Fraction.newInstance(3, 1);
     Fraction areaInsideCircle = radius.times(radius).times(approximationOfPI);
