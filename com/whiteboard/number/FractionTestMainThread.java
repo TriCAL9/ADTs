8eaef83585dbc05f9459cb638e6943f4545ad292
Calvin Francis <calvinfran@gmail.com>
2023-07-30 15:00:12 +0000

    Create a test for plus and minus

--- com/whiteboard/number/FractionTestMainThread.java
+++ com/whiteboard/number/FractionTestMainThread.java
@@ -7,8 +7,20 @@ public class FractionTestMainThread {
     Fraction approximationOfPI= Fraction.newInstance(22, 7);
     Fraction radius = Fraction.newInstance(3, 1);
     Fraction areaInsideCircle = radius.times(radius).times(approximationOfPI);
+    int a = 1;
+    int b = 2; 
+    int c = 3;
+    Fraction quadraticSolutionForX = Fraction.newInstance(-b, 1)
+    .plus(Fraction.newInstance((int) (StrictMath.pow(b,2) - 4*a*c), 2*a));
+    Fraction negativQuadraticSolutionForX = Fraction.newInstance(-b, 1)
+    .minus(Fraction.newInstance((int) (StrictMath.pow(b, 2) - 4*a*c), 2*a));
     System.out.println(areaInsideCircle);
     System.out.println("Approximated area: " + areaInsideCircle.getNumerator() 
-                       / areaInsideCircle.getDenominator());
+    / areaInsideCircle.getDenominator());
+    System.out.println("ax^2+x+3 = 0 x1 = " + quadraticSolutionForX + " x2 = "
+     + negativQuadraticSolutionForX);
+    System.out.println(Fraction.ONE_HALF.plus(Fraction.ONE_QUARTER).toString());
+    System.out.println(Fraction.ONE_HALF.minus(Fraction.ONE_THIRD));
+    
   }
 }
